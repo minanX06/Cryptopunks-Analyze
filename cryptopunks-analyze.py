0a6931c028104bd3198076e13a6697b769628c1f
minanX06 <96944902+minanX06@users.noreply.github.com>
2022-05-28 19:54:22 +0900

    Add files via upload

---
+++ cryptopunks-analyze.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+
+crypto = []
+cryp2 = []
+req = requests.get("https://cryptopunks.app/cryptopunks/attributes")
+soup = BeautifulSoup(req.text, "html.parser")
+for anchor in soup.select("tr.center.attribute-row"):
+    crypto.append(str(anchor))
+
+for i in range(5):
+    crypto.pop(0)
+
+for k in range(0, 87):
+    att = crypto[k].split("<td>")[2:5]
+
+    for j in range(0, 3):
+        cryp2.append(att[j].split("\n")[1].strip())
+
+wb = Workbook()
+ws = wb.active
+
+for i in range(87):
+    ws["A{}".format(i + 1)] = cryp2[i*3]
+    ws["B{}".format(i + 1)] = cryp2[i*3 + 2][:-1]
+
+wb.save("cryptopunks-analyze.xlsx")
+wb.close()
